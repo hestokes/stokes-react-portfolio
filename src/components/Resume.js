5bdf7ac5fba390ce15d8a6c174b9a37fcfb448d2
Aaron Stokes <stokescomp01@gmail.com>
2022-06-09 21:21:00 -0600

    build resume.js, build work.js

---
+++ src/components/Resume.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../styles/Resume.css";
+
+function Resume() {
+  return (
+    <section className="resume">
+      <nav id="resume">
+        <h2>Resume</h2>
+      </nav>
+      <button>
+        <a href="https://www.linkedin.com/in/h-aaron-stokes/">
+          {" "}
+          Click me to see my resume{" "}
+        </a>
+      </button>
+    </section>
+  );
+}
+
+export default Resume;
---
+++ src/components/Work.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "../styles/Work.css";
+
+function Work() {
+  return (
+    <section className="work">
+      <nav id="work">
+        <h2>Work</h2>
+      </nav>
+      <div className="workbox">
+        <button id="workboxbtn">
+          {" "}
+          <a href="https://jeongholee21.github.io/weather-dashboard/">
+            <img src={require("../styles/images/weather.jpg")} />
+            <section className="details">
+              <h4> Weather Dashboard! </h4>
+              <p>
+                Deployed Page: https://hestokes.github.io/weatherdashboard/{" "}
+              </p>
+            </section>
+          </a>
+        </button>
+
+        <button id="workboxbtn">
+          {" "}
+          <a href="https://jeongholee21.github.io/work-day-scheduler/index.html">
+            <img src={require("../styles/images/scheduler.jpg")} />
+            <section className="details">
+              <h4> Work Day Scheduler </h4>
+              <p>
+                Deployed Page: https://hestokes.github.io/work-day-scheduler/{" "}
+              </p>
+            </section>
+          </a>
+        </button>
+
+        <button id="workboxbtn">
+          {" "}
+          <a href="https://big-sexy-recipes.herokuapp.com/">
+            <img src={require("../styles/images/recipe.jpg")} />
+            <section className="details">
+              <h4>Sexy Recipes</h4>
+              <p>Deployed Page: https://big-sexy-recipes.herokuapp.com/ </p>
+            </section>{" "}
+          </a>
+        </button>
+      </div>
+    </section>
+  );
+}
+
+export default Work;
